460cfa6a2285a2c182d94a4138d4de549e4c9ebf
Abhay <abhay.kumar02@sap.com>
2017-05-22 09:58:11 +0000

    shell script weak quotes corrections

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,19 +4,19 @@ cd /var/lib/postgresql
 
 # Initialize data directory
 DATA_DIR=/data/postgres
-if [ ! -f $DATA_DIR/postgresql.conf ]; then
-    mkdir -p $DATA_DIR
-    chown postgres:postgres $DATA_DIR
+if [ ! -f "$DATA_DIR"/postgresql.conf ]; then
+    mkdir -p "$DATA_DIR"
+    chown postgres:postgres "$DATA_DIR"
 
-    sudo -u postgres /usr/lib/postgresql/9.4/bin/initdb -E utf8 --locale en_US.UTF-8 -D $DATA_DIR
+    sudo -u postgres /usr/lib/postgresql/9.4/bin/initdb -E utf8 --locale en_US.UTF-8 -D "$DATA_DIR"
     sed -i -e"s/^#listen_addresses =.*$/listen_addresses = '*'/" $DATA_DIR/postgresql.conf
-    echo  "shared_preload_libraries='pg_stat_statements'">> $DATA_DIR/postgresql.conf
-    echo "host    all    all    0.0.0.0/0    md5" >> $DATA_DIR/pg_hba.conf
+    echo  "shared_preload_libraries='pg_stat_statements'">> "$DATA_DIR"/postgresql.conf
+    echo "host    all    all    0.0.0.0/0    md5" >> "$DATA_DIR"/pg_hba.conf
 
-    mkdir -p $DATA_DIR/pg_log
+    mkdir -p "$DATA_DIR"/pg_log
 fi
-chown -R postgres:postgres $DATA_DIR
-chmod -R 700 $DATA_DIR
+chown -R postgres:postgres "$DATA_DIR"
+chmod -R 700 "$DATA_DIR"
 
 # Initialize first run
 if [[ -e /.firstrun ]]; then
@@ -25,4 +25,4 @@ fi
 
 # Start PostgreSQL
 echo "Starting PostgreSQL..."
-exec chpst sudo -u postgres /usr/lib/postgresql/9.4/bin/postgres -D $DATA_DIR
+exec chpst sudo -u postgres /usr/lib/postgresql/9.4/bin/postgres -D "$DATA_DIR"
